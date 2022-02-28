482ef6d35ea3e8a7ac65af71fb165be529ca4dcd
ashennova <kennerve14@gmail.com>
2022-02-28 17:12:37 +0800

    reversed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :exam_dates
-  root 'exam_dates#index'
+  root 'index#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
